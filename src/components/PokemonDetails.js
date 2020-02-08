8d8ada0d6b6fbe3bd6859214bbcb7e10faec95bd
Vanessa <vanessa@pebbleroad.com>
2020-02-09 02:14:34 +0800

    Feat(Details Page): Get pokemon details

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPokemonDetails } from "../store/actions";
+import axios from "axios";
 
 function PokemonDetails(props) {
-    let { id } = useParams();
+    const { allPokemons } = props;
+    const { id } = useParams();
 
-    return <div>asdf {id}</div>;
+    const [pokemon, setPokemon] = useState();
+    const pokemonUrl = allPokemons[id];
+
+    useEffect(() => {
+        axios.get(pokemonUrl).then(res => {
+            setPokemon(res.data);
+        });
+    }, []);
+
+    const catchPokemon = () => {
+        console.log("try catch");
+    };
+
+    return (
+        <div>
+            {pokemon && (
+                <div>
+                    <div>{pokemon.name}</div>
+                    <div>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                    </div>
+                    <div>
+                        {pokemon.types.map((entry, index) => (
+                            <div key={index}>{entry.type.name}</div>
+                        ))}
+                    </div>
+                    <button onClick={catchPokemon}>Catch</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default PokemonDetails;
+const mapStateToProps = state => ({
+    allPokemons: state.allPokemons,
+    myPokemons: state.mine.pokemons
+});
+
+export default connect(mapStateToProps, { fetchPokemonDetails })(PokemonDetails);

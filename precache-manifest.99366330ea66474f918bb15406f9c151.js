587c15eb63550ce498453098997a8b64fc53121b
Vanessa <vanessa@pebbleroad.com>
2020-02-12 20:41:39 +0800

    Updates

---
+++ precache-manifest.99366330ea66474f918bb15406f9c151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165a0834d9f776302ba57812f1b8004f",
+    "url": "/pokemon-list/index.html"
+  },
+  {
+    "revision": "f56e98ed65e586af6e69",
+    "url": "/pokemon-list/static/css/main.77869cf2.chunk.css"
+  },
+  {
+    "revision": "3a86386e123c1f213528",
+    "url": "/pokemon-list/static/js/2.8977fe6d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pokemon-list/static/js/2.8977fe6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56e98ed65e586af6e69",
+    "url": "/pokemon-list/static/js/main.5f9af398.chunk.js"
+  },
+  {
+    "revision": "2b4939cc3151e5afa89f",
+    "url": "/pokemon-list/static/js/runtime-main.95a6482c.js"
+  },
+  {
+    "revision": "3d71502e9fca88bf19a67f51dd74be49",
+    "url": "/pokemon-list/static/media/icon-preloading.3d71502e.gif"
+  }
+]);
